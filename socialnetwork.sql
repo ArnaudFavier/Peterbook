36f332e8857ea49d4946f28602d51430062e720b
Dortouffy <cab.tommy@gmail.com>
2015-02-09 10:29:59 +0100

    modifications, i hope it will work

--- socialnetwork.sql
+++ socialnetwork.sql
@@ -3,8 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Server Version :  5.6.20
--- PHP Version :  5.5.15
+-- Généré le :  Lun 09 Février 2015 à 10:17
+-- Version du serveur :  5.6.20
+-- Version de PHP :  5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -16,37 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- DataBase :  `Peterbook`
+-- Base de données :  `peterbook`
 --
-CREATE DATABASE IF NOT EXISTS `Peterbook` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `Peterbook`;
 
 -- --------------------------------------------------------
 
 --
--- Structure of `post` table
---
-
-DROP TABLE IF EXISTS `post`;
-CREATE TABLE IF NOT EXISTS `post` (
-`IDPOST` int(11) NOT NULL,
-  `USERNAME` varchar(20) NOT NULL,
-  `CONTENT` varchar(2000) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
---
--- Content of `post` table
---
-
-INSERT INTO `post` (`IDPOST`, `USERNAME``, `CONTENT`) VALUES
-(3, 'testUsername', 'testContent');
-
-
--- --------------------------------------------------------
-
-
---
--- Structure of `ci_sessions` table
+-- Structure de la table `ci_sessions`
 --
 
 CREATE TABLE IF NOT EXISTS `ci_sessions` (
@@ -54,23 +31,19 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
   `ip_address` varchar(16) NOT NULL DEFAULT '0',
   `user_agent` varchar(120) NOT NULL,
   `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
-  `user_data` text NOT NULL,
-  PRIMARY KEY (`session_id`),
-  KEY `last_activity_idx` (`last_activity`)
+  `user_data` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
-
 --
--- Structure of `comment` table
+-- Structure de la table `comment`
 --
 
-DROP TABLE IF EXISTS `comment`;
 CREATE TABLE IF NOT EXISTS `comment` (
 `IDCOMMENT` int(11) NOT NULL,
-  `USERNAME` varchar(30) NOT NULL,
-  `IDPOST` int(11) NOT NULL,
+  `USERNAMECOMMENT` varchar(30) NOT NULL,
+  `ID_POST` int(11) NOT NULL,
   `TEXT` varchar(500) NOT NULL,
   `DATE` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
@@ -78,57 +51,90 @@ CREATE TABLE IF NOT EXISTS `comment` (
 -- --------------------------------------------------------
 
 --
--- Structure of `user` table
+-- Structure de la table `post`
 --
 
-DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `post` (
+`IDPOST` int(11) NOT NULL,
+  `USERNAMEPOST` varchar(20) NOT NULL,
+  `CONTENT` varchar(2000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
 CREATE TABLE IF NOT EXISTS `user` (
-  `USER_NAME` varchar(20) NOT NULL,
+  `USERNAME` varchar(20) NOT NULL,
   `EMAIL` varchar(30) NOT NULL,
   `PASSWORD` varchar(50) NOT NULL,
   `DESCRIPTION` varchar(100) NOT NULL,
   `PICTURE` varchar(50) NOT NULL,
   `FIRSTNAME` varchar(20) NOT NULL,
-  `LASTNAME` varchar(20) NOT NULL,
+  `LASTNAME` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Content of `user` table
+-- Index pour les tables exportées
 --
 
-INSERT INTO `user` (`USER_NAME`, `EMAIL`, `PASSWORD`, `DESCRIPTION`, `PICTURE`, `FIRSTNAME`, `LASTNAME`) VALUES
-('aTestUser', 'hello@hello.com', 'password1236456987', 'hello, im a user !', 'pic.jpg', 'tommy', 'cabrelli'),
-
+--
+-- Index pour la table `ci_sessions`
+--
+ALTER TABLE `ci_sessions`
+ ADD PRIMARY KEY (`session_id`), ADD KEY `last_activity_idx` (`last_activity`);
 
 --
--- Index for `post` table
---
-ALTER TABLE `post`
- ADD PRIMARY KEY (`IDPOST`), ADD KEY `FK_REFERENCE_1` (`USERNAME`);
-
-
---
--- Index for `comment` table
+-- Index pour la table `comment`
 --
 ALTER TABLE `comment`
- ADD PRIMARY KEY (`IDCOMMENT`), ADD KEY `UserToComment` (`USERNAME`), ADD KEY `PostToComment` (`IDPOST`);
+ ADD PRIMARY KEY (`IDCOMMENT`), ADD UNIQUE KEY `FOREIGN` (`USERNAMECOMMENT`), ADD UNIQUE KEY `FOREIGN2` (`ID_POST`);
 
 --
--- Index for `user` table
---
-ALTER TABLE `user`
- ADD PRIMARY KEY (`USERNAME``);
-
---
--- AUTO_INCREMENT for `post` table
+-- Index pour la table `post`
 --
 ALTER TABLE `post`
-MODIFY `IDPOST` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+ ADD PRIMARY KEY (`IDPOST`), ADD UNIQUE KEY `FOREIGN` (`USERNAMEPOST`);
+
 --
--- AUTO_INCREMENT for `comment` table
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`USERNAME`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `comment`
 --
 ALTER TABLE `comment`
 MODIFY `IDCOMMENT` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `post`
+--
+ALTER TABLE `post`
+MODIFY `IDPOST` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `comment`
+--
+ALTER TABLE `comment`
+ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`ID_POST`) REFERENCES `post` (`IDPOST`),
+ADD CONSTRAINT `username` FOREIGN KEY (`USERNAMECOMMENT`) REFERENCES `user` (`USERNAME`);
+
+--
+-- Contraintes pour la table `post`
+--
+ALTER TABLE `post`
+ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`USERNAMEPOST`) REFERENCES `user` (`USERNAME`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
