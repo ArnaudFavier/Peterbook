dc57e06e7390a1e2a22baef6d51420759d52459b
DorTouffy <cab.tommy@gmail.com>
2015-02-09 09:14:46 +0100

    DataBase Made !

--- socialnetwork.sql
+++ socialnetwork.sql
@@ -1,11 +1,10 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Ven 06 Février 2015 à 10:19
--- Version du serveur :  5.6.17
--- Version de PHP :  5.5.12
+-- Server Version :  5.6.20
+-- PHP Version :  5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,25 +16,101 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données :  `socialnetwork`
+-- DataBase :  `Peterbook`
 --
+CREATE DATABASE IF NOT EXISTS `Peterbook` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `Peterbook`;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `ci_sessions`
+-- Structure of `post` table
 --
 
-CREATE TABLE IF NOT EXISTS `ci_sessions` (
-  `session_id` varchar(40) NOT NULL DEFAULT '0',
-  `ip_address` varchar(16) NOT NULL DEFAULT '0',
-  `user_agent` varchar(120) NOT NULL,
-  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
-  `user_data` text NOT NULL,
-  PRIMARY KEY (`session_id`),
-  KEY `last_activity_idx` (`last_activity`)
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE IF NOT EXISTS `post` (
+`IDPOST` int(11) NOT NULL,
+  `USERNAME` varchar(20) NOT NULL,
+  `CONTENT` varchar(2000) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Content of `post` table
+--
+
+INSERT INTO `post` (`IDPOST`, `USERNAME``, `CONTENT`) VALUES
+(3, 'testUsername', 'testContent');
+
+-- --------------------------------------------------------
+
+
+--
+-- Structure of `comment` table
+--
+
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE IF NOT EXISTS `comment` (
+`IDCOMMENT` int(11) NOT NULL,
+  `USERNAME` varchar(30) NOT NULL,
+  `IDPOST` int(11) NOT NULL,
+  `TEXT` varchar(500) NOT NULL,
+  `DATE` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure of `user` table
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `USER_NAME` varchar(20) NOT NULL,
+  `EMAIL` varchar(30) NOT NULL,
+  `PASSWORD` varchar(50) NOT NULL,
+  `DESCRIPTION` varchar(100) NOT NULL,
+  `PICTURE` varchar(50) NOT NULL,
+  `FIRSTNAME` varchar(20) NOT NULL,
+  `LASTNAME` varchar(20) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Content of `user` table
+--
+
+INSERT INTO `user` (`USER_NAME`, `EMAIL`, `PASSWORD`, `DESCRIPTION`, `PICTURE`, `FIRSTNAME`, `LASTNAME`) VALUES
+('aTestUser', 'hello@hello.com', 'password1236456987', 'hello, im a user !', 'pic.jpg', 'tommy', 'cabrelli'),
+
+
+--
+-- Index for `post` table
+--
+ALTER TABLE `post`
+ ADD PRIMARY KEY (`IDPOST`), ADD KEY `FK_REFERENCE_1` (`USERNAME`);
+
+
+--
+-- Index for `comment` table
+--
+ALTER TABLE `comment`
+ ADD PRIMARY KEY (`IDCOMMENT`), ADD KEY `UserToComment` (`USERNAME`), ADD KEY `PostToComment` (`IDPOST`);
+
+--
+-- Index for `user` table
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`USERNAME``);
+
+--
+-- AUTO_INCREMENT for `post` table
+--
+ALTER TABLE `post`
+MODIFY `IDPOST` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for `comment` table
+--
+ALTER TABLE `comment`
+MODIFY `IDCOMMENT` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
