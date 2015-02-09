718125fecaa56b2d980eaeda019984ee3f7359bd
DorTouffy <cab.tommy@gmail.com>
2015-02-09 09:19:01 +0100

    added 'Ci Sessions'

--- socialnetwork.sql
+++ socialnetwork.sql
@@ -41,6 +41,24 @@ CREATE TABLE IF NOT EXISTS `post` (
 INSERT INTO `post` (`IDPOST`, `USERNAME``, `CONTENT`) VALUES
 (3, 'testUsername', 'testContent');
 
+
+-- --------------------------------------------------------
+
+
+--
+-- Structure of `ci_sessions` table
+--
+
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+  `session_id` varchar(40) NOT NULL DEFAULT '0',
+  `ip_address` varchar(16) NOT NULL DEFAULT '0',
+  `user_agent` varchar(120) NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_data` text NOT NULL,
+  PRIMARY KEY (`session_id`),
+  KEY `last_activity_idx` (`last_activity`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- --------------------------------------------------------
 
 
