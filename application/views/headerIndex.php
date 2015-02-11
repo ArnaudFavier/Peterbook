a192cd8eb6c202ce2939588e52f4228137b51e70
ArnaudCode <jeux.arnaud@orange.fr>
2015-02-11 10:29:42 +0100

    Add errors messages for login and little design (color name fields)

--- application/views/headerIndex.php
+++ application/views/headerIndex.php
@@ -2,8 +2,8 @@
 	<div class="in_titlebar">
 		<img class="logo" src="assets/img/logo_without_background.png" alt="logo" height="110" width="250"/>
 		<form class="connexion" method="post" action="">
-			<label>Login: </label><input type="text" name="username" />
-			<label>Password: </label><input type="password" name="password" />
+			<label>Login: </label><input type="text" name="username" /><?php echo form_error('username'); ?>
+			<label>Password: </label><input type="password" name="password" /><?php echo form_error('password'); ?>
 			<input type="submit" value="Connexion" />
 		</form>
 	</div>
--- application/views/headerLogin.php
+++ application/views/headerLogin.php
@@ -1,5 +1,10 @@
-<div class="titlebar">
-	<img src="assets/img/logo_without_background.png" alt="logo" height="110" width="250"> 
+<div class="titlebar_background">
+	<div class="in_titlebar">
+		<img class="logo" src="assets/img/logo_without_background.png" alt="logo" height="110" width="250"/>
+		<form class="logout" method="post" action="">
+			<input type="submit" value="Logout" />
+		</form>
+	</div>
 </div>
 <!-- Add a little shadow -->
 <div class="after">
