7c030629973d8ebbf245922154ad7d3a076b17fd
Dortouffy <cab.tommy@gmail.com>
2015-02-11 09:58:12 +0100

    style modifications

--- application/views/headerIndex.php
+++ application/views/headerIndex.php
@@ -1,10 +1,12 @@
-<div class="titlebar">
-	<img src="assets/img/logo_without_background.png" alt="logo" height="110" width="250"> 
-	<form>
-		<label>Login: </label><input type="text" name="login" />
-		<label>Password: </label><input type="password" nme="password" />
-		<input type="submit" value="Connexion" />
-	</from>
+<div class="titlebar_background">
+	<div class="in_titlebar">
+		<img class="logo" src="assets/img/logo_without_background.png" alt="logo" height="110" width="250"/>
+		<form class="connexion">
+			<label>Login: </label><input type="text" name="login" />
+			<label>Password: </label><input type="password" nme="password" />
+			<input type="submit" value="Connexion" />
+		</form>
+	</div>
 </div>
 <!-- Add a little shadow -->
 <div class="after">
