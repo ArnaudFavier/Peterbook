3d83ff22ba84386bbfd4f1ad4f0600d7450eaa7c
ADussauge <aurelien.dussauge@etu.univ-lyon1.fr>
2015-02-23 12:26:15 +0100

    Creation of the subscribe page

--- application/views/index.php
+++ application/views/index.php
@@ -1,3 +1,43 @@
-<h1>Peterbook</h1>
-
-<h2>Under Construction</h2>
+<div class="inscription_page">
+	<table>
+		<tr>
+			<td>
+				<div class="inscription_main_text">
+					Peterbook allows you to keep in touch with your friends even if you're as sick as Florian Le Brech.
+				</div>
+				<img src="assets/img/world.png" alt="world image">
+			</td>
+			<td>
+				<form class="subscribe_form">
+				<table>
+					<tr>
+						<td colspan="2" class='subscribe'>Subscribe</td>
+					</tr>
+					<tr>
+						<td colspan="2" class='free'>It's free (except for Corentin Marechal)</td>
+					</tr>
+					<tr>					
+						<td width='199'><input type='text' class='subscribe_input_name' placeholder='First name'></input></td>
+						<td><input type='text' class='subscribe_input_name' placeholder='Last name'></input></td>
+					</tr>
+					<tr>
+						<td colspan="2"><input type='text' class='subscribe_input' placeholder='Email'></input></td>
+					</tr>
+					<tr>
+						<td colspan="2"><input type='text' class='subscribe_input' placeholder='Confirm your email'></input></td>
+					</tr>
+					<tr>
+						<td colspan="2"><input type='text' class='subscribe_input' placeholder='Password'></input></td>
+					</tr>
+					<tr>
+						<td colspan="2" class='prevention'>By clicking on Subscribe, you accept our cookie's utilisation, to give your soul to Satan, and many other condition.</td>
+					</tr>
+					<tr>
+						<td colspan="2"><input type='submit' class='subscribe_button' placeholder='Email'></input></td>
+					</tr>
+				</table>
+				</form>
+			</td>
+		</tr>
+	</table>
+</div>
