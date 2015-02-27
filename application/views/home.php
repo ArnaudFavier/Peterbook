c54ad8b46b97d9cd66d804341d980e14b5730d40
ArnaudCode <jeux.arnaud@orange.fr>
2015-02-27 10:19:35 +0100

    Better style for see all posts In previous commit, the readme was modified

--- application/views/home.php
+++ application/views/home.php
@@ -1,27 +1,33 @@
-<h1>Welcome <?php echo $username; ?></h1>
-<form method="post" action="post/addPost">
-
-		<table class='post'>
-			<tr>
-				<td class='top_post'>
-					zdazdazdaz
-				</td>
-			</tr>
-			<tr >
-				<td class='bottom_post'>
-				azdazdzdazda
-				</td>
-			</tr>
-		</table>
-
-	<p>
-		<label for="content">What's new?</label><br />
-		<textarea name="content" id="content"></textarea><br />
-		<input class ="button" type="submit" value="Post" />
-	</p>
-</form>
+<table class='post'>
+	<tr>
+		<td class='top_post'>
+			<h2>Welcome <?php echo $username; ?></h2>
+		</td>
+	</tr>
+	<tr >
+		<td class='bottom_post'>
+			<form method="post" action="post/addPost">
+				<label for="content">What's new?</label><br />
+				<textarea name="content" id="content"></textarea><br />
+				<input class ="button" type="submit" value="Post" />
+			</form>	
+		</td>
+	</tr>
+</table>
+<br />
+<?php foreach($allPosts as $numPost => $post) { ?>
+	<table class='post'>
+		<tr>
+			<td class='top_post'>
+				<?php echo $post->USERNAMEPOST . " said: "; ?>
+			</td>
+		</tr>
+		<tr >
+			<td class='bottom_post'>
+				<?php  echo $post->CONTENT; ?>
+			</td>
+		</tr>
+	</table>
+	<br />
+<?php } ?>
 <hr>
-<p>All posts from <span id="peterbookTitle">Peterbook</span>:</p>
-<?php foreach($allPosts as $numPost => $post) {
-	echo $post->USERNAMEPOST . " said: " . $post->CONTENT . "<br />";
-} ?>
