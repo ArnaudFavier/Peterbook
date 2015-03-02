99f3568b7c6f56428b3045b34d5a44ea503161fa
ADussauge <aurelien.dussauge@etu.univ-lyon1.fr>
2015-03-02 11:40:44 +0100

    Add editprofile.php

--- application/views/headerLogin.php
+++ application/views/headerLogin.php
@@ -1,13 +1,20 @@
 <div class="titlebar_background">
 	<div class="in_titlebar">
-		<img class="logo" src="assets/img/logo_without_background.png" alt="logo" height="110" width="250"/>
-		<form class="logout" method="post" action="logout">
+		<img class="logo" src="assets/img/logo_without_background.png" alt="logo" height="110" width="250"/>	
+		<form class="logout" method="post" action="editprofile">
 			<table>
 				<tr>
 					<td><input class ="button" type="submit" value="Logout" /></td>
 				</tr>
 			</table>
-		</form>
+		</form>	
+		<form class="logout" method="post" action="logout">
+			<table>
+				<tr>
+					<td><input class ="button" type="submit" value="Edit profile" /></td>
+				</tr>
+			</table>
+		</form>		
 	</div>
 </div>
 <!-- Add a little shadow -->
