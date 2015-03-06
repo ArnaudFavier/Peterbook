4f279179cdf8fc22b006fef3c265202246026e7a
Dortouffy <cab.tommy@gmail.com>
2015-03-06 09:58:35 +0100

    editprofile.php : has no errors anymore

--- application/controllers/editprofile.php
+++ application/controllers/editprofile.php
@@ -13,15 +13,16 @@ class Editprofile extends CI_Controller
 		$this->layout->setTitre('Edit profile.');
 		$this->layout->views('headerLogin')
 					->view('editprofile');
+		$this->load->library('form_validation');
 					
-	}	$this->form_validation->set_error_delimiters('<p class="form_error">', '</p>');
+		$this->form_validation->set_error_delimiters('<p class="form_error">', '</p>');
 
 		$this->form_validation->set_rules('picture',  '"Picture"',  'trim|required|min_length[3]|max_length[256]|alpha_dash');
 		$this->form_validation->set_rules('description', '"Description"', 'trim|required|min_length[3]|max_length[256]');
 		
 		if($this->form_validation->run())
 		{
-			$return = $this->profile_model->editUserProfile($this->session->userdata('email')
+			$return = $this->profile_model->editUserProfile($this->session->userdata('email'),
 															$this->input->post('picture'),
 															$this->input->post('description'));
 			
@@ -34,4 +35,5 @@ class Editprofile extends CI_Controller
 		redirect('/index/');
 		
 		
+	}
 }
