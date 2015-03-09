b35a95669922e9abcb4164be636e1bef62c4d820
ADussauge <aurelien.dussauge@etu.univ-lyon1.fr>
2015-03-09 09:28:32 +0100

    edit profile page

--- application/views/editprofile.php
+++ application/views/editprofile.php
@@ -1,1 +1,24 @@
-<h1>Edit Profile</h1>
+<div class="center_div">
+	<h2>Edit your profile</h2>
+	
+	<form class="editprofile" method="post" action="editprofile/">
+		<table>
+			<tr>
+				<td><img class='picture' src='assets/img/photo.jpg' alt='photo de profil'/></td>
+				<td class='center'>
+					Change your profile picture</br>
+					<input type="file" name="photo" src='assets/img/photo.jpg'></input>
+				</td>	
+			</tr>
+			<tr>
+				<td class="humor" colspan="2">
+					Edit your humor message : 
+					<input type='text' class='subscribe_input' placeholder='Humor message' name='email2'>
+				</td>	
+			</tr>
+		</table>
+	
+		<input type='submit' class='subscribe_button' value='Submit'/>
+	
+	</form>
+</div>
